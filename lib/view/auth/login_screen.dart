52bd4b80cc491bc3f85e841f5746b2d590ccda63
AfilAhamed <afilahamed0007@gmail.com>
2024-05-02 10:04:12 +0530

    first commit

---
+++ lib/view/auth/login_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Center(
+                  child: Image.asset(
+                'assets/images/login_screen_image.png',
+                height: mq.height * 0.2,
+                width: mq.width * 0.4,
+                fit: BoxFit.contain,
+              )),
+              SizedBox(
+                height: mq.height * 0.050,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 7),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Enter Phone Number',
+                    style: GoogleFonts.montserrat(
+                        fontSize: 18, fontWeight: FontWeight.w600),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: mq.height * 0.02,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+                  hintText: 'Enter Phone Number *',
+                  hintStyle: GoogleFonts.montserrat(
+                      color: Colors.grey,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Colors.grey,
+                        width: 1,
+                      )),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Colors.black,
+                        width: 2,
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: mq.height * 0.03,
+              ),
+              RichText(
+                text: TextSpan(
+                  style: GoogleFonts.montserrat(
+                      color: Colors.grey.shade600,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600),
+                  children: [
+                    const TextSpan(text: 'By Continuing, I agree to TotalX’s '),
+                    TextSpan(
+                      text: 'Terms and condition',
+                      style: TextStyle(color: Colors.blue.shade300),
+                    ),
+                    const TextSpan(text: ' & '),
+                    TextSpan(
+                      text: 'privacy Policy',
+                      style: TextStyle(color: Colors.blue.shade300),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: mq.height * 0.04,
+              ),
+              SizedBox(
+                  height: mq.height * 0.066,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      style: const ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.black)),
+                      onPressed: () {},
+                      child: Text(
+                        'Get OTP',
+                        style: GoogleFonts.montserrat(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      )))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
